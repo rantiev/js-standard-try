7612d395783e929ac901aece25af173b624be820
Pavel <rantie86@gmail.com>
2017-08-09 22:06:42 +0300

    Add trailing comma

--- index.js
+++ index.js
@@ -4,29 +4,29 @@
  * @returns {number}
  */
 function fun1(a = 1, b = 2) {
-  return a + b;
+  return a + b
 }
 
-let x = 3;
-let y = 4;
-let z = 5;
+let x = 3
+let y = 4
+let z = 5
 
-fun1();
-fun1(x, y);
+fun1()
+fun1(x, y)
 
 if (x === y) {
   console.log(
-    x + "long long long long long long long long long long long string"
-  );
+    x + "long long long long long long long long long long long string",
+  )
 }
 
 if (z === 5) {
-  x = z;
+  x = z
 }
 
 let myPromise = new Promise((resolve, reject) => {
-  resolve(1);
-});
+  resolve(1)
+})
 
 myPromise
   .then(r => r)
@@ -37,10 +37,10 @@ myPromise
   .then(r => r)
   .then(r => r)
   .catch(e => e)
-  .finally(() => {});
+  .finally(() => {})
 
 function manyParamsCheck(param1, param2, param3, param4, param5) {
-  console.log(param1);
+  console.log(param1)
 }
 
 function trailingCommasCheck(
@@ -53,17 +53,17 @@ function trailingCommasCheck(
   param7,
   param8,
   param9,
-  param10
+  param10,
 ) {
-  console.log(param1);
+  console.log(param1)
 }
 
-let longNamedVariable1 = 1;
-let longNamedVariable2 = 1;
-let longNamedVariable3 = 1;
-let longNamedVariable4 = 1;
-let longNamedVariable5 = 1;
-let longNamedVariable6 = 1;
+let longNamedVariable1 = 1
+let longNamedVariable2 = 1
+let longNamedVariable3 = 1
+let longNamedVariable4 = 1
+let longNamedVariable5 = 1
+let longNamedVariable6 = 1
 
 if (
   longNamedVariable1 ||
@@ -73,5 +73,5 @@ if (
   longNamedVariable5 ||
   longNamedVariable6
 ) {
-  console.log("Something!");
+  console.log("Something!")
 }
